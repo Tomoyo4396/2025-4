b6b8c7e74ddbbe0755c82ebb27e30a9713437477
Tomoyo4396 <463659267@qq.com>
2025-04-12 14:13:56 +0800

    Add files via upload
    一些修改

---
+++ Wait for me.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//为使用printf而引用的头文件
+#include <stdio.h>
+//main函数：主函数；一个工程中只能有一个main函数.
+int main()
+//main函数以下部分：体函数；int：整形
+{
+	printf("hello world\n");
+	return 0;
+}
+
